668ebdef163a559da829b94c475e29c6b8c8e893
nate <natsukingly@gmail.com>
2023-12-05 09:52:07 +0900

    fix

--- next/utils/apiClient.ts
+++ next/utils/apiClient.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log(process.env.API_ENDOPOINT);
 export const apiClient = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_ENDOPOINT,
     responseType: 'json',
